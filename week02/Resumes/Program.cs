ca5b96e236dcb413366d904093c1d7ddbf33621b
HarryPotter777ZZZZZ <HarryPotter777ZZZZZ@gmail.com>
2025-01-17 00:05:29 +0300

    Midweek Assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -6,9 +6,12 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
 
-        job1._jobTitle = "Software Engineer"
+        job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1.Disblay();
 
         myResume._jobs[0]._jobTitle
+        resume.Display();
 
     }
 }
