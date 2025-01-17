f2bb46a50e39d663364a03246ab1f41ac80210a2
HarryPotter777ZZZZZ <HarryPotter777ZZZZZ@gmail.com>
2025-01-17 14:46:12 +0300

    Job1 Job2 added

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
@@ -6,12 +7,26 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
 
-        job1 = new Job();
+        Job job1 = new Job();
+        // job1 instantiation
         job1._jobTitle = "Software Engineer";
-        job1.Disblay();
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        job1.Display();
+        // job2 instantiation
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        job2.Display();
 
-        resume1 = myResume._jobs[0]._jobTitle
-        resume.Display();
+        // Resume instantiation
+        // Resume resume = new Resume();
+
+
+
 
     }
 }
