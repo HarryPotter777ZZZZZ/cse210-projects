928abe820a510b74c5919d22621ab9bd0b34f4de
HarryPotter777ZZZZZ <HarryPotter777ZZZZZ@gmail.com>
2025-01-22 23:22:17 +0300

    Fraction class in a separate file

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -6,45 +6,6 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
 
-        public class Fraction
-        {
-            private int _top;
-            private int _bottom;
-
-            public Fraction()
-            {
-                // Default to 1/1
-                _top = 1;
-                _bottom = 1;
-            }
-
-            public Fraction(int wholeNumber)
-            {
-                _top = wholeNumber;
-                _bottom = 1;
-            }
-
-            public Fraction(int top, int bottom)
-            {
-                _top = top;
-                _bottom = bottom;
-            }
-
-            public string GetFractionString()
-            {
-                // Notice that this is not stored as a member variable.
-                // Is is just a temporary, local variable that will be recomputed each time this is called.
-                string text = $"{_top}/{_bottom}";
-                return text;
-            }
-
-    public double GetDecimalValue()
-    {
-        // Notice that this is not stored as a member variable.
-        // Is will be recomputed each time this is called.
-        return (double)_top / (double)_bottom;
-    }
-}
         Fraction f1 = new Fraction();
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
