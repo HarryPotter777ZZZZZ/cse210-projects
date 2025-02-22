ccacaddcfd67bb66caddfbbc7cba0546dd2daf1e
HarryPotter777ZZZZZ <HarryPotter777ZZZZZ@gmail.com>
2025-02-22 19:24:46 +0300

    Shapes prog added

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -8,27 +8,27 @@ class Program
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
 
-    {
-        Reference reference = new Reference("John", 3, 16);
-        Scripture scripture = new Scripture(reference, "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
-
-        Console.WriteLine("Original Scripture:");
-        Console.WriteLine(scripture.GetDisplayText());
-
-        while (!scripture.IsCompletelyHidden())
         {
-            Console.WriteLine("\nPress Enter to hide a random word or type 'quit' to exit.");
-            string input = Console.ReadLine();
-            if (input.ToLower() == "quit")
-            {
-                break;
-            }
-            scripture.HideRandomWords(1);
-            Console.WriteLine(scripture.GetDisplayText());
-        }
+            Reference reference = new Reference("John", 3, 16);
+            Scripture scripture = new Scripture(reference, "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
 
-        Console.WriteLine("\nAll words are hidden. Program ended.");
-    }
+            Console.WriteLine("Original Scripture:");
+            Console.WriteLine(scripture.GetDisplayText());
+
+            while (!scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine("\nPress Enter to hide a random word or type 'quit' to exit.");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                {
+                    break;
+                }
+                scripture.HideRandomWords(1);
+                Console.WriteLine(scripture.GetDisplayText());
+            }
+
+            Console.WriteLine("\nAll words are hidden. Program ended.");
+        }
     }
 }
 
